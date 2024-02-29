9bbab38842ed5195b42876baaaddaf9e4cc8a159
Patricia Segantine <pnsegantine@gmail.com>
2024-02-29 10:49:21 +0000

    refactor: add type at input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,13 +4,14 @@ import { InputContainer } from "./input.styles.ts";
 interface InputProps {
   icon?: any
   placeholder: string
+  type: React.HTMLInputTypeAttribute
 }
 
-export const Input: React.FC<InputProps> = ({icon: Icon, placeholder}) => {
+export const Input: React.FC<InputProps> = ({icon: Icon, placeholder, type}) => {
   return (
     <InputContainer>
       {Icon && <Icon size={20}/>}
-      <input placeholder={placeholder}/>
+      <input placeholder={placeholder} type={type}/>
     </InputContainer>
   );
 };
