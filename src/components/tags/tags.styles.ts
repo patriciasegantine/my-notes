5cb5ef53892b89899f6993d93da9010b65f8e3c2
Patricia Segantine <pnsegantine@gmail.com>
2024-02-29 10:52:39 +0000

    style: change font-size at tags

--- src/components/tags/tags.styles.ts
+++ src/components/tags/tags.styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const TagContainer = styled.span`
 
-  font-size: ${({theme}) => theme.FONT.EXTRA_SMALL};
+  font-size: ${({theme}) => theme.FONT.SMALL};
   padding: 5px 14px;
   border-radius: ${({theme}) => theme.BORDER.RADIUS_SMALL};
   margin-right: ${({theme}) => theme.SPACING.EXTRA_SMALL};
